1a026e64f1d575aa6234ee2735b372961de3de1a
sadd <sadaly204@gmail.com>
2025-06-27 09:52:04 +0300

    Добавлены файлы миграции, предудыщая База данных была дропнута.

---
+++ Persistence/Migrations/20250627064943_BaseEntityDatesAndSoftDeleteFlagAdded.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class BaseEntityDatesAndSoftDeleteFlagAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Email_Value = table.Column<string>(type: "text", nullable: false),
+                    PasswordHashed_Value = table.Column<string>(type: "text", nullable: false),
+                    Username_Value = table.Column<string>(type: "text", nullable: false),
+                    IsSoftDelete = table.Column<bool>(type: "boolean", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdateAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "User");
+        }
+    }
+}
