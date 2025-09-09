d69bb2e52ec9533b84532a7f1f0c515f6fb22a81
LAPTOP-JF958PDO\mm121 <mm12112004@mail.ru>
2025-09-09 12:39:08 +0300

    Добавил комментарий к большому коммиту (выполняется в рамках лабораторной работы)

--- Domain/Abstractions/IEntity.cs
+++ Domain/Abstractions/IEntity.cs
@@ -4,6 +4,10 @@ namespace Domain.Abstractions
 {
 	internal interface IEntity
 	{
+		/// <summary>
+		/// Метод отмечает сущность для удаления. Это позволит восстанавливать данные на уровне бд
+		/// </summary>
+		/// <returns></returns>
 		public Result SoftDelete();
 	}
 }
